f50e88c4b3b956c9579fbc60a3199b49231fefd4
George Laing <george@pxadventures.co.uk>
2023-12-19 14:44:28 +0000

    commit

---
+++ PXC-ImportAssist.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Plugin Name:       PX Commerce Import Assistant
+ * Plugin URI:        https://pxcommerce.co.uk/plugins/pxcommerce-importassistant
+ * Description:       A plugin to extend the capabilities of WooCommerce's builtin CSV Importer
+ * Version:           1.0.0
+ * Author:            PXCommerce
+ * Author URI:        https://pxcommerce.co.uk/
+ * License:           GPL v3 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       pxc-importassistant
+ */
+
+
+
+ 
+?>
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# PXCommerce-ImportAssistant
-A plugin to extend the capabilities of WooCommerce's builtin CSV Importer
-
+=== PXCommerce-ImportAssistant ===
+Tags: tag1, tag2
+Requires at least: 4.7
+Tested up to: 5.4
+Stable tag: 4.3
+Requires PHP: 7.0
+License: GPLv3 or later
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+A plugin to extend the capabilities of WooCommerce's builtin CSV Importer.
